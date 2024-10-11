1066cf3177de47c74f3a13fe73ecb68551802bc1
ignat <1gnatvaschilo@gmail.com>
2024-10-11 16:28:24 +0300

    part1 heap

---
+++ part1-v2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand((unsigned int) time(NULL));
+    int n;
+    cin >> n;
+    int *p_arr = new int [n];
+    cout << "Исходный массив: ";
+    for (int i = 0; i < n; i++) {
+        p_arr[i] = rand() % 100;
+        cout << p_arr[i] << " ";
+    }
+
+    long result = -1;
+    for (int i = 0; i < n; i++) {
+        if (p_arr[i]*p_arr[i] > result) {
+            result = p_arr[i]*p_arr[i];
+        }
+    }
+    cout << endl << "Максимальный квадрат числа = " << result;
+    return 0;
+}
